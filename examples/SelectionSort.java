88583a9d568894ae7c79b6aa9ad59279726d1ecd
davidcok <davidcok@github.com>
2026-01-20 09:59:31 -0500

    Fixing SelectionSort

--- examples/SelectionSort.java
+++ examples/SelectionSort.java
@@ -4,15 +4,15 @@
 public class SelectionSort {    
     /*@
         assigns arr[*];
-        ensures \forall int k;0 <= k && k < arr.length-1;arr[k] >= arr[k+1];
+        ensures \forall int k; 0 <= k && k < arr.length-1; arr[k] >= arr[k+1];
     @*/
+    // Sorts the array in-place from maximum down to minimum
     public static void sort(int /*@ non_null @*/ [] arr) {
         //@ ghost final int n = arr.length;
         
         //@ loop_invariant 0 <= i <= n; // Bounds check
-        /*@ loop_invariant \forall int j; 0 <= j < i; // Sorted up-to i
-                               \forall int k; j < k < n; arr[j] >= arr[k];
-        @*/
+        //@ loop_invariant \forall int j; 0 <= j < i; // Sorted up-to i
+        //@                    \forall int k; j < k < n; arr[j] >= arr[k];
         //@ loop_assigns i, arr[*];
         //@ decreasing n-i; // i goes up
         for (int i = 0; i < arr.length; i++) {
@@ -24,7 +24,7 @@ public class SelectionSort {
             
             //@ loop_invariant i < j <= n; // Bounds check
             //@ loop_invariant \forall int k; i <= k < j; ub >= arr[k]; // max elem up-to j
-            //@ loop_invariant i <= l < n; // max index bounds check
+            //@ loop_invariant i <= l < j; // max index bounds check
             //@ loop_invariant ub == arr[l]; // max index corresponds to max elem.
             //@ loop_assigns j, l, ub;
             //@ decreasing n-j; // j goes up
