09a76c9ca89d8f3898e6c58833c1c2e970d246a2
davidcok <davidcok@github.com>
2026-02-26 14:05:12 -0500

    Examples fixes

--- tutorial/T_PureMethod1.java
+++ tutorial/T_PureMethod1.java
@@ -10,7 +10,7 @@ public class T_PureMethod1 {
 
     //@ requires max >= 0;
     //@ ensures count == 0 && maxCount == max;
-    //@ spec_pure
+    //@ pure
     public Counter(int max) {
       count = 0;
       maxCount = max;
