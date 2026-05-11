79cd6af175fec7206b22de055fe8c4d4a150d898
Gary T. Leavens <Leavens@ucf.edu>
2026-05-11 14:38:40 -0400

    Adding files

---
+++ tutorial/T_NullableList.java
@@ -0,0 +1,16 @@
+// openjml --esc T_NullableList.java
+interface T_NullableList {
+    /*@ model instance non_null Object elem; @*/
+    /*@ model instance nullable T_NullableList tail; @*/
+
+    //@ ensures \result <==> ls == null;
+    static boolean isEmpty(T_NullableList ls) {
+        return ls == null;
+    }
+    
+    //@ ensures \result == this.elem;
+    /*@ non_null @*/ Object head();
+
+    /*@ ensures this.tail != null ==> \result == this.tail; @*/
+    /*@ nullable @*/ T_NullableList tail();
+}
