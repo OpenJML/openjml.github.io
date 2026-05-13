0f1b46c79166ec4560bf9406cf3aa07ca106981c
Gary T. Leavens <Leavens@ucf.edu>
2026-05-13 12:06:24 -0400

    Clarifications

--- tutorial/T_LoopInitError.java
+++ tutorial/T_LoopInitError.java
@@ -1,14 +1,14 @@
-// openjml --esc T_LoopInitError.java
+// openjml --no-show-summary --esc T_LoopInitError.java
 public class T_LoopInitError {
 
   public void setToRamp(int[] a) {
     //@ maintaining 0 < i <= a.length;
-    //@ maintaining \forall int k; 0 <= k < i; a[k] == k;
+    //@ maintaining (\forall int k; 0 <= k < i; a[k] == k);
     //@ loop_writes i, a[*];
     //@ decreases a.length -i;
     for (int i = 0; i < a.length; i++) {
         a[i] = i;
     }
-    //@ assert \forall int i; 0 <= i < a.length; a[i] == i;
+    //@ assert (\forall int i; 0 <= i < a.length; a[i] == i);
   }
 }
