f489766a80da06eca76c2bc9fafe111ecc9a414c
Gary T. Leavens <Leavens@ucf.edu>
2026-05-13 11:28:22 -0400

    formatting

--- tutorial/T_forloop.java
+++ tutorial/T_forloop.java
@@ -1,11 +1,12 @@
-// openjml --esc T_forloop.java
+// openjml --no-show-summary --esc T_forloop.java
 public class T_forloop {
 
+  //@ requires a.length > 0;
   public void setToRamp(int[] a) {
     //@ maintaining 0 <= i <= a.length;
-    //@ maintaining \forall int k; 0 <= k < i; a[k] == k;
+    //@ maintaining (\forall int k; 0 <= k < i; a[k] == k);
     //@ loop_writes i, a[*];
-    //@ decreases a.length -i;
+    //@ decreases a.length - i;
     for (int i = 0; i < a.length; i++) {
         a[i] = i;
     }
