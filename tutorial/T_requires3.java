7a55c40e04f5c085ce87e3a5cfa3b8568e4219f1
Gary T. Leavens <Leavens@ucf.edu>
2026-05-25 14:28:53 -0400

    reordering requires claues to match text

--- tutorial/T_requires3.java
+++ tutorial/T_requires3.java
@@ -1,8 +1,8 @@
 // openjml --esc --nullable-by-default T_requires3.java
 public class T_requires3 {
 
-  //@ requires 0 <= index < a.length;
   //@ requires a != null;
+  //@ requires 0 <= index < a.length;
   //@ ensures \result == a[index];
   public int getElement(int[] a, int index) {
     return a[index];
