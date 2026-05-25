4c1222831de946f737754911b3a3b145a4e2e160
Gary T. Leavens <Leavens@ucf.edu>
2026-05-25 14:29:59 -0400

    restoring this to match text

--- tutorial/T_requires3.java
+++ tutorial/T_requires3.java
@@ -1,8 +1,8 @@
 // openjml --esc --nullable-by-default T_requires3.java
 public class T_requires3 {
 
-  //@ requires a != null;
   //@ requires 0 <= index < a.length;
+  //@ requires a != null;  // out of order!
   //@ ensures \result == a[index];
   public int getElement(int[] a, int index) {
     return a[index];
