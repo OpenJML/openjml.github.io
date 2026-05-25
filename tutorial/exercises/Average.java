d798877ee9733c6f7eb9ec6670cf694c98f8efea
Gary T. Leavens <Leavens@ucf.edu>
2026-05-25 17:39:11 -0400

    Adding these

--- tutorial/exercises/Average.java
+++ tutorial/exercises/Average.java
@@ -1,8 +1,6 @@
 public class Average {
 
-    /*@ requires !Double.isNaN(x);
-      @ requires !Double.isNaN(y);
-      @ ensures Math.abs(\result - (x+y)/2.0) < 0.001; @*/
+    /*@ ensures Math.abs(\result - (x+y)/2.0) < 0.001; @*/
     public double average(double x, double y) {
         return (x+y)/2.0;
     }
