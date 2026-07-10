1ef3c35ef74200ff22c003f8ac1da0dac40c1355
Gary T. Leavens <Leavens@ucf.edu>
2026-07-10 13:20:34 -0400

    Clarifications, but it seems to work forever in ESC

--- tutorial/T_Old.java
+++ tutorial/T_Old.java
@@ -7,16 +7,15 @@ public class T_Old {
   //@ ensures \result == g+g+g;
   public int myGCD(int x, int y) {
      // some code
-     int g = mygcd(x,y);
-     return g+g+g;
+     int gcd = mygcd(x,y);
+     return gcd+gcd+gcd;
   }
 
   //@ requires x > 0 && y > 0;
   //@ old \bigint g = Math.gcd(x,y); // spec from Math library
-  //@ requires g+g+g <= Integer.MAX_VALUE;
   //@ ensures \result == Math.gcd(x,y); // spec from Math library
   //@ pure
   public int mygcd(int x, int y) {
-      return 0; // placeholder
+      return 1; // placeholder
   }
 }
