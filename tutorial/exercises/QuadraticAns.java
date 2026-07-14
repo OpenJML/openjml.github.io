01ff01bb5b8023beb02e745c096f7c47304b8cb7
Gary T. Leavens <Leavens@ucf.edu>
2026-07-14 11:21:31 -0400

    Use another old clause for epsilon

--- tutorial/exercises/QuadraticAns.java
+++ tutorial/exercises/QuadraticAns.java
@@ -13,12 +13,13 @@ public class QuadraticAns {
         second = s;
     }
 
+    //@ old double epsilon = 0.1e-9;
     //@ old double fps = first+second;
     //@ old double discrim = fps*fps - 4.0 * (first*second);
     //@ requires 0.0 < discrim;
     //@ ensures \result.length == 2;
-    //@ ensures Math.abs(\result[0] - (-fps + Math.sqrt(discrim) / 2.0)) < 0.1e-9;
-    //@ ensures Math.abs(\result[1] - (-fps - Math.sqrt(discrim) / 2.0)) < 0.1e-9;
+    //@ ensures Math.abs(\result[0] - (-fps + Math.sqrt(discrim) / 2.0)) < epsilon;
+    //@ ensures Math.abs(\result[1] - (-fps - Math.sqrt(discrim) / 2.0)) < epsilon;
     //@ pure
     public double[] roots() {
         double res[] = new double[2];
